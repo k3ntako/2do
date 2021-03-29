b15cb8d3a18bef3757af61c510c345d5c4b8c78a
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-29 11:03:00 -0400

    Add database migration to create todo table

---
+++ server/src/main/resources/db/migration/V2__Create_todo_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE todo (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    description varchar(255),
+    is_complete boolean not null DEFAULT false,
+    due_date date,
+    user_name varchar(255)
+);
