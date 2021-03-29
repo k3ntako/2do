812a0ec2fcb4b97f86c27e589bd565d7676b46e4
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-03-29 13:50:34 -0400

    Create Todo model

--- server/src/main/java/com/kentaromatt/todoapi/ToDo.java
+++ server/src/main/java/com/kentaromatt/todoapi/ToDo.java
@@ -1,35 +1,44 @@
 package com.kentaromatt.todoapi;
 
-import java.time.LocalDate;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 public class ToDo {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id = UUID.randomUUID();
     private String description;
-    private Boolean isComplete;
-    private LocalDate dueDate;
+    private Boolean is_complete = false;
+    private LocalDate due_date;
+    private String user_name;
 
     protected ToDo() {}
-    public ToDo(String description) {
+
+    public ToDo(String description, LocalDate due_date, String user_name) {
         this.description = description;
-        this.isComplete = false;
+        this.due_date = due_date;
+        this.user_name = user_name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "ToDo[id='%s', description='%s', is_complete='%s', due_date='%s', user_name='%s']",
+                id, description, is_complete, due_date, user_name);
     }
 
     public ToDo(String description, LocalDate dueDate) {
         this.description = description;
-        this.isComplete = false;
-        this.dueDate = dueDate;
+        this.is_complete = false;
+        this.due_date = dueDate;
     }
 
-    public Long getId() {
+    public UUID getId() {
         return id;
     }
 
@@ -38,10 +47,14 @@ public class ToDo {
     }
 
     public Boolean getIsComplete() {
-        return isComplete;
+        return is_complete;
     }
 
     public LocalDate getDueDate() {
-        return dueDate;
+        return due_date;
+    }
+
+    public String getUserName() {
+        return user_name;
     }
 }
--- server/src/main/java/com/kentaromatt/todoapi/ToDoRepository.java
+++ server/src/main/java/com/kentaromatt/todoapi/ToDoRepository.java
@@ -1,11 +1,13 @@
 package com.kentaromatt.todoapi;
 
 import java.util.List;
+import java.util.UUID;
+
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ToDoRepository extends CrudRepository<ToDo, Long> {
-    
+public interface ToDoRepository extends CrudRepository<ToDo, UUID> {
+    ToDo findById(String id);
     List<ToDo> findByDescription(String description);
     List<ToDo> findAll();
 }
