9863cd3caaceea42cdaf1096772926b43e84fea5
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-04-07 13:03:08 -0400

    Update production backend URL

--- client/src/config.ts
+++ client/src/config.ts
@@ -14,10 +14,11 @@ const config: IConfig = {
   },
   staging: {
     SERVER_URL:
-      "http://todobackend-staging.eba-pinzma4i.us-west-1.elasticbeanstalk.com",
+      "http://todobackend-staging.us-west-1.elasticbeanstalk.com/health-check",
   },
   production: {
-    SERVER_URL: "",
+    SERVER_URL:
+      "http://todobackend-production.us-west-1.elasticbeanstalk.com/health-check",
   },
 };
 
