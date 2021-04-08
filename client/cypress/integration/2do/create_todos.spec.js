e2ec10fefc41504be7b5b1f88d69ae3135e4e69f
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-04-08 17:12:28 -0400

    Draft Cypress tests

--- client/cypress/integration/2do/create_todos.spec.js
+++ client/cypress/integration/2do/create_todos.spec.js
@@ -5,6 +5,18 @@ describe("Listing todos on page load", () => {
     cy.visit("/");
   });
 
+  // TODO: Remove after frontend and backend are integrated
+  beforeEach(() => {
+    cy.fixture("todo/POST-200").then((body) => {
+      cy.intercept("POST", /\/api\/todos/, {
+        statusCode: 200,
+        body,
+      });
+    });
+
+    cy.visit("/");
+  });
+
   describe("user loads ", () => {
     it("should display create todo form", () => {
       cy.get("#create-todo-form h2").contains("Create a Todo");
@@ -22,6 +34,10 @@ describe("Listing todos on page load", () => {
       );
 
       cy.get("#create-todo-form button").contains("Submit").click();
+
+      cy.get("#card-content")
+        .contains("div.todo-container", "Run Boston marathon")
+        .as("todo");
     });
   });
 });
