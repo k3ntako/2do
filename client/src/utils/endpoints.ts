50b906c55a89c610420a83a01a72b3a6a24303d6
Matt Readout <41033651+digitopolis@users.noreply.github.com>
2021-04-08 18:26:28 -0700

    Add endpoint, CORS config for POST method

--- client/src/utils/endpoints.ts
+++ client/src/utils/endpoints.ts
@@ -3,4 +3,5 @@ import config from "../config";
 export const ApiEndpoints = {
   get: () => `${config.SERVER_URL}/api/todos`,
   update: (todoId: string) => `${config.SERVER_URL}/api/todo/${todoId}`,
+  create: () => `${config.SERVER_URL}/api/todos`,
 };
